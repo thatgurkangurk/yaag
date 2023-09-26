32535f90e789c6ff4c08b2aa885271f2e9d81b6d
Gurkan <hello@gurkz.me>
2023-09-26 16:38:01 +0200

    prevent the player from going off the screen

--- scripts/Player.cs
+++ scripts/Player.cs
@@ -14,11 +14,31 @@ namespace Game.Player
         [Export]
         float acceleration = 10.0f;
 
+        /// <summary>
+        /// prevent the player from going outside of the viewport
+        /// </summary>
+        /// <param name="viewportSize">the viewport size</param>
+        private void preventOutsideMovement(Vector2 viewportSize)
+        {
+            if (GlobalPosition.Y < 0)
+            {
+                // player is above the screen
+                GlobalPosition = GlobalPosition with { Y = viewportSize.Y };
+            }
+            else if (GlobalPosition.Y > viewportSize.Y)
+            {
+                GlobalPosition = GlobalPosition with { Y = 0 };
+            }
+        }
+
         public override void _PhysicsProcess(double delta)
         {
             Vector2 inputVector = new Vector2(0, Input.GetAxis(PlayerInput.Forward, PlayerInput.Backward));
             Velocity += inputVector * acceleration;
             MoveAndSlide();
+
+            Vector2 viewportSize = GetViewportRect().Size;
+            preventOutsideMovement(viewportSize);
         }
     }
 
